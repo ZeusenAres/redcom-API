efa88068b10aeb541e5b83634b01866b59fd3fda
ZeusenAres <2060537@talnet.nl>
2022-10-09 11:30:01 +0200

    added spring boot project foundation

---
+++ src/main/java/com/redcom/redcom/RedcomApplication.java
@@ -0,0 +1,13 @@
+package com.redcom.redcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redcom/redcom/RedcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redcom.redcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
